305d5a30c19a9037845c8bc2741b299ae02d83ab
J-C-L-G <ps_jclg@hotmail.com>
2020-05-11 15:03:03 -0700

    Listing 1-30. Including Inquirier and prompting the user for input

--- 1 Chapter/dist/index.js
+++ 1 Chapter/dist/index.js
@@ -2,22 +2,40 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const TodoItem_1 = require("./TodoItem");
 const TodoCollection_1 = require("./TodoCollection");
+const inquirer_1 = require("inquirer");
+var Commands;
+(function (Commands) {
+    Commands["Quit"] = "Quit";
+    Commands["Add"] = "Add";
+})(Commands || (Commands = {}));
 const todos = [
     new TodoItem_1.TodoItem(1, "Learn TypeScript"),
     new TodoItem_1.TodoItem(2, "Learn Mobx"),
     new TodoItem_1.TodoItem(3, "Learn Loopback"),
     new TodoItem_1.TodoItem(4, "Learn Hooks"),
+    new TodoItem_1.TodoItem(5, "Learn React")
 ];
 const collection = new TodoCollection_1.TodoCollection("Carlos", todos);
-const todoId = collection.addTodo("Learn React");
-const reactTodo = collection.getTodoById(todoId);
-const todoCount = collection.getItemCounts();
-console.clear();
-console.log(`${collection.userName}'s Todo List`);
-console.log(`Total Todo's: ${todoCount.total}, Pending tasks: ${todoCount.incomplete}`);
-console.log(reactTodo.printDetails());
-collection.markComplete(todoId, true);
-console.log(collection.getTodoItems(true));
-collection.removeComplete();
-console.log(collection.getTodoItems(true));
-console.log(reactTodo.printDetails());
+const displayTodoList = (collection) => {
+    const todoCount = collection.getItemCounts();
+    console.log(`${collection.userName}'s Todo List`);
+    console.log(`Total Todo's: ${todoCount.total}, Pending tasks: ${todoCount.incomplete}`);
+    collection.getTodoItems(false).forEach(todo => todo.printDetails());
+};
+const promptUser = async () => {
+    console.clear();
+    displayTodoList(collection);
+    const answer = await inquirer_1.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose an option",
+        choices: Object.values(Commands)
+    });
+    if (answer["command"] !== Commands.Quit) {
+        return promptUser();
+    }
+    else {
+        return answer;
+    }
+};
+promptUser().then(console.log);
--- 1 Chapter/src/index.ts
+++ 1 Chapter/src/index.ts
@@ -1,23 +1,43 @@
 import {TodoItem} from "./TodoItem";
 import {TodoCollection} from "./TodoCollection";
+import {prompt} from "inquirer";
+
+enum Commands {
+    Quit = "Quit",
+    Add = "Add",
+}
 
 const todos: TodoItem[] = [
     new TodoItem(1, "Learn TypeScript"),
     new TodoItem(2, "Learn Mobx"),
     new TodoItem(3, "Learn Loopback"),
     new TodoItem(4, "Learn Hooks"),
+    new TodoItem(5, "Learn React")
 ];
 const collection: TodoCollection = new TodoCollection("Carlos", todos);
-const todoId = collection.addTodo("Learn React");
-const reactTodo = collection.getTodoById(todoId);
-const todoCount = collection.getItemCounts();
 
-console.clear();
-console.log(`${collection.userName}'s Todo List`);
-console.log(`Total Todo's: ${todoCount.total}, Pending tasks: ${todoCount.incomplete}`);
-console.log(reactTodo.printDetails());
-collection.markComplete(todoId, true);
-console.log(collection.getTodoItems(true));
-collection.removeComplete();
-console.log(collection.getTodoItems(true));
-console.log(reactTodo.printDetails());
+const displayTodoList = (collection: TodoCollection): void => {
+    const todoCount = collection.getItemCounts();
+    console.log(`${collection.userName}'s Todo List`);
+    console.log(`Total Todo's: ${todoCount.total}, Pending tasks: ${todoCount.incomplete}`);
+    collection.getTodoItems(false).forEach(todo => todo.printDetails());
+};
+
+const promptUser = async (): Promise<object> => {
+    console.clear();
+    displayTodoList(collection);
+    const answer = await prompt({
+        type: "list",
+        name: "command",
+        message: "Choose an option",
+        choices: Object.values(Commands)
+    });
+
+    if (answer["command"] !== Commands.Quit) {
+        return promptUser();
+    } else {
+        return answer;
+    }
+};
+
+promptUser().then(console.log);
