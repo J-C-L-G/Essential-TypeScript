29cda415d8a9baa7c9f21750b7e19a5d66513f3c
J-C-L-G <ps_jclg@hotmail.com>
2020-05-10 22:47:42 -0700

    Listing 1-11 concise class properties

--- 1 Chapter/dist/TodoItem.js
+++ 1 Chapter/dist/TodoItem.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// ..:: Concise ::..
+// Allows the TodoItem class to receive parameters and use them to create instance properties in a single step.
 class TodoItem {
     constructor(id, task, complete = false) {
         this.id = id;
@@ -11,3 +13,20 @@ class TodoItem {
     }
 }
 exports.TodoItem = TodoItem;
+// ..:: Verbose ::..
+/*
+export class TodoItem {
+    public id: number;
+    public task: string;
+    public complete: boolean;
+
+    public constructor(id: number, task: string, complete: boolean = false){
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+    }
+
+    public printDetails(): void{
+        console.log(`${this.id}\t${this.task}\t Completed: "${this.complete}"`);
+    }
+}*/
--- 1 Chapter/src/TodoItem.ts
+++ 1 Chapter/src/TodoItem.ts
@@ -1,3 +1,19 @@
+// ..:: Concise ::..
+// Allows the TodoItem class to receive parameters and use them to create instance properties in a single step.
+export class TodoItem {
+    constructor(public id: number,
+                public task: string,
+                public complete: boolean = false) {
+    }
+
+    public printDetails(): void{
+        console.log(`${this.id}\t${this.task}\t Completed: "${this.complete}"`);
+    }
+}
+
+
+// ..:: Verbose ::..
+/*
 export class TodoItem {
     public id: number;
     public task: string;
@@ -12,4 +28,4 @@ export class TodoItem {
     public printDetails(): void{
         console.log(`${this.id}\t${this.task}\t Completed: "${this.complete}"`);
     }
-}
+}*/
