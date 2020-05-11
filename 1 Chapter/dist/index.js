eb057cb6afbfe7fb933cfaf13a2659cbd616404b
J-C-L-G <ps_jclg@hotmail.com>
2020-05-11 16:39:38 -0700

    Listing 1-38. Completing Items and Purging list in the index.ts File in the src Folder

--- 1 Chapter/dist/index.js
+++ 1 Chapter/dist/index.js
@@ -5,23 +5,32 @@ const TodoCollection_1 = require("./TodoCollection");
 const inquirer_1 = require("inquirer");
 var Commands;
 (function (Commands) {
+    Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Add"] = "Add Mew Task";
+    Commands["Complete"] = "Complete Task";
+    Commands["Purge"] = "Remove Completed Tasks";
     Commands["Quit"] = "Quit";
-    Commands["Add"] = "Add";
 })(Commands || (Commands = {}));
 const todos = [
     new TodoItem_1.TodoItem(1, "Learn TypeScript"),
     new TodoItem_1.TodoItem(2, "Learn Mobx"),
     new TodoItem_1.TodoItem(3, "Learn Loopback"),
     new TodoItem_1.TodoItem(4, "Learn Hooks"),
-    new TodoItem_1.TodoItem(5, "Learn React")
+    new TodoItem_1.TodoItem(5, "Learn React", true)
 ];
 const collection = new TodoCollection_1.TodoCollection("Carlos", todos);
+let showCompletedTodo = false;
 const displayTodoList = (collection) => {
     const todoCount = collection.getItemCounts();
     console.log(`${collection.userName}'s Todo List`);
     console.log(`Total Todo's: ${todoCount.total}, Pending tasks: ${todoCount.incomplete}`);
-    collection.getTodoItems(false).forEach(todo => todo.printDetails());
+    collection.getTodoItems(showCompletedTodo).forEach(todo => todo.printDetails());
 };
+const promptInput = (message) => inquirer_1.prompt({
+    type: "input",
+    name: "data",
+    message
+});
 const promptUser = async () => {
     console.clear();
     displayTodoList(collection);
@@ -31,11 +40,33 @@ const promptUser = async () => {
         message: "Choose an option",
         choices: Object.values(Commands),
     });
-    if (answer.command !== Commands.Quit) {
-        return promptUser();
+    switch (answer.command) {
+        case Commands.Quit: {
+            return answer;
+        }
+        case Commands.Add: {
+            const { data } = await promptInput("Enter task:");
+            if (data) {
+                collection.addTodo(data);
+            }
+            break;
+        }
+        case Commands.Complete: {
+            const { data } = await promptInput("Enter task ID:");
+            if (data) {
+                collection.markComplete(+data, true);
+            }
+            break;
+        }
+        case Commands.Toggle: {
+            showCompletedTodo = !showCompletedTodo;
+            break;
+        }
+        case Commands.Purge: {
+            collection.removeComplete();
+            break;
+        }
     }
-    else {
-        return answer;
-    }
+    return promptUser();
 };
 promptUser().then(console.log);
--- 1 Chapter/src/index.ts
+++ 1 Chapter/src/index.ts
@@ -2,9 +2,20 @@ import {TodoItem} from "./TodoItem";
 import {TodoCollection} from "./TodoCollection";
 import {prompt} from "inquirer";
 
+type CommandObject = {
+    command: string
+};
+
+type UserData = {
+    data: string
+}
+
 enum Commands {
+    Toggle = "Show/Hide Completed",
+    Add = "Add Mew Task",
+    Complete = "Complete Task",
+    Purge = "Remove Completed Tasks",
     Quit = "Quit",
-    Add = "Add",
 }
 
 const todos: TodoItem[] = [
@@ -12,18 +23,25 @@ const todos: TodoItem[] = [
     new TodoItem(2, "Learn Mobx"),
     new TodoItem(3, "Learn Loopback"),
     new TodoItem(4, "Learn Hooks"),
-    new TodoItem(5, "Learn React")
+    new TodoItem(5, "Learn React", true)
 ];
 const collection: TodoCollection = new TodoCollection("Carlos", todos);
+let showCompletedTodo: boolean = false;
 
 const displayTodoList = (collection: TodoCollection): void => {
     const todoCount = collection.getItemCounts();
     console.log(`${collection.userName}'s Todo List`);
     console.log(`Total Todo's: ${todoCount.total}, Pending tasks: ${todoCount.incomplete}`);
-    collection.getTodoItems(false).forEach(todo => todo.printDetails());
+    collection.getTodoItems(showCompletedTodo).forEach(todo => todo.printDetails());
 };
 
-const promptUser = async (): Promise<object> => {
+const promptInput = (message: string): Promise<UserData> => prompt({
+    type: "input",
+    name: "data",
+    message
+});
+
+const promptUser = async (): Promise<CommandObject> => {
     console.clear();
     displayTodoList(collection);
     const answer = await prompt({
@@ -33,11 +51,34 @@ const promptUser = async (): Promise<object> => {
         choices: Object.values(Commands),
     });
 
-    if (answer.command !== Commands.Quit) {
-        return promptUser();
-    } else {
-        return answer;
+    switch (answer.command) {
+        case Commands.Quit: {
+            return answer;
+        }
+        case Commands.Add: {
+            const {data} = await promptInput("Enter task:");
+            if (data) {
+                collection.addTodo(data);
+            }
+            break;
+        }
+        case Commands.Complete: {
+            const {data} = await promptInput("Enter task ID:");
+            if (data) {
+                collection.markComplete(+data, true);
+            }
+            break;
+        }
+        case Commands.Toggle: {
+            showCompletedTodo = !showCompletedTodo;
+            break;
+        }
+        case Commands.Purge: {
+            collection.removeComplete();
+            break;
+        }
     }
+    return promptUser();
 };
 
 promptUser().then(console.log);
