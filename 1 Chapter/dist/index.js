1c926bfad78a2ca23cf36f39ae281c18cb830fe1
J-C-L-G <ps_jclg@hotmail.com>
2020-05-10 22:37:53 -0700

    Listing 1-10 class properties similar to Java

---
+++ 1 Chapter/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const TodoItem_1 = require("./TodoItem");
+console.clear();
+console.log("Carlos's Todo List");
+console.log((new TodoItem_1.TodoItem(1, "test")).printDetails());
---
+++ 1 Chapter/src/index.ts
@@ -0,0 +1,5 @@
+import {TodoItem} from "./TodoItem"
+
+console.clear();
+console.log("Carlos's Todo List");
+console.log((new TodoItem(1, "test")).printDetails());
