008a8641718e55847916d565b5438541b0b0fb33
J-C-L-G <ps_jclg@hotmail.com>
2020-05-11 11:03:25 -0700

    1-16 Adding types to index

--- 1 Chapter/src/index.ts
+++ 1 Chapter/src/index.ts
@@ -1,13 +1,13 @@
 import {TodoItem} from "./TodoItem";
 import {TodoCollection} from "./TodoCollection";
 
-const todos = [
+const todos: TodoItem[] = [
     new TodoItem(1, "Learn TypeScript"),
     new TodoItem(2, "Learn Mobx"),
     new TodoItem(3, "Learn Loopback"),
     new TodoItem(4, "Learn Hooks"),
 ];
-const collection = new TodoCollection("Carlos", todos);
+const collection: TodoCollection = new TodoCollection("Carlos", todos);
 const todoId = collection.addTodo("Learn React");
 const reactTodo = collection.getTodoById(todoId);
 
